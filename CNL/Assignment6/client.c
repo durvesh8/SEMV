716b3e25e727c21ede786f2c2e2f009488b777d1
Your Name <durvesh@example.com>
2020-10-10 06:56:13 +0530

    finished A6 CNL

--- CNL/Assignment6/client.c
+++ CNL/Assignment6/client.c
@@ -12,6 +12,7 @@ filename server_ipaddress portno
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <ctype.h>
 
 
 void error(const char *msg) {
@@ -23,74 +24,87 @@ int main(int argc, char const *argv[]) {
     int sockfd,portno,n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
-    printf("d1\n");
     char buffer[255];
     if(argc<3){
-        fprintf(stderr, "usage %s hostname port\n",argv[0]);
+        fprintf(stderr, "usage %s hostname port\nport 9890 for chat\nport 9891 for file transfer\nport 9892 for calculator\n",argv[0]);
         exit(1);
     }
-    portno = atoi(argv[2]);
+    portno = atoi(argv[2]);     // String to integer
     sockfd = socket(AF_INET,SOCK_STREAM,0);
     if(sockfd < 0)
     error("Error opening Socket");
-    printf("d2\n");
     server = gethostbyname(argv[1]);
     if(server == NULL)
     fprintf(stderr,"Error, no such host");
-    printf("d3\n");
     bzero((char *)&serv_addr,sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     bcopy((char*)server->h_addr,(char *)&serv_addr.sin_addr.s_addr,server->h_length);
     serv_addr.sin_port = htons(portno);   // host to network short
-    printf("d4\n");
-    if(connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr))<0){
+    if(connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr))<0){      // Connecting to Server
         error("Connection failed");
     }
-    printf("it's here?");
-    if(portno==9890){
+    if(portno==9890){           // Chat App
         while(1){
             bzero(buffer,255);
-            fgets(buffer,255,stdin);
+            fgets(buffer,255,stdin);    // Reading from client terminal
             int j = strncmp("bye",buffer,3);
             if (j==0)
             break;
-            n = write(sockfd,buffer,strlen(buffer));
+            n = write(sockfd,buffer,strlen(buffer));        // Sending to server
             if(n<0)
             error("Error on writing");
             bzero(buffer,255);
-            n = read(sockfd,buffer,255);
+            n = read(sockfd,buffer,255);                    // Reading from server
             if(n<0)
             error("Error on reading");
-            printf("Server says: %s\n",buffer);
+            printf("Server says: %s\n",buffer);             // Printing server's sent message
             int i = strncmp("bye",buffer,3);
             if (i==0)
             break;
         }
     }
-    if(portno==9891){
+    if(portno==9891){       // File Transfer
+        FILE *f;
+        int words = 0;
+        char c;
+        f = fopen("textfile.txt","r");      // Opening an already existing file to send its data to server
+        while((c= getc(f))!= EOF){          // Counting the number of words
+            fscanf(f,"%s",buffer);
+            if(isspace(c)||c=='\t')
+            words++;
+        }
+        write(sockfd,&words,sizeof(int));       // Sending the number of words
+        rewind(f);          // Setting the file pointer at initial word
+
+        char ch;
+        while(ch!=EOF){
+            fscanf(f,"%s",buffer);
+            write(sockfd,buffer,255);       // Sending words one by one
+            ch = fgetc(f);
+        }
+        printf("The file has been sent.\n");
 
     }
     if(portno==9892){
         while(1){
-            printf("It's here alright\n");
             int num1,num2,choice,answer;
             bzero(buffer,255);
-            n = read(sockfd,buffer,255);
+            n = read(sockfd,buffer,255);            // Reading first request from server
             if(n<0)
                 error("Error reading");
-            printf("Server- %s",buffer);
-            scanf("%d", &num1);
-            n = write(sockfd,&num1,sizeof(int));
+            printf("Server- %s",buffer);            // printing first request
+            scanf("%d", &num1);                     // getting num1 from client terminal
+            n = write(sockfd,&num1,sizeof(int));        // sending num1 to server
             if (n<0) {
                 error("Error writing\n");
             }
             bzero(buffer,255);
-            n = read(sockfd,buffer,255);
+            n = read(sockfd,buffer,255);            // Reading first request from server
             if(n<0)
             error("Error reading");
-            printf("Server- %s",buffer);
-            scanf("%d", &num2);
-            n = write(sockfd,&num2,sizeof(int));
+            printf("Server- %s",buffer);            // printing first request
+            scanf("%d", &num2);                         // getting num1 from client terminal
+            n = write(sockfd,&num2,sizeof(int));        // sending num2 to server
             if (n<0) {
                 error("Error writing\n");
             }
@@ -99,19 +113,19 @@ int main(int argc, char const *argv[]) {
             if(n<0)
             error("Error reading");
             printf("Server- %s\n",buffer);
-            scanf("%d", &choice);
+            scanf("%d", &choice);               // Reading operation choice from client
 
             n = write(sockfd,&choice,sizeof(int));
             if (n<0) {
                 error("Error writing\n");
             }
-            if(choice==5){
+            if(choice==5){      // Exit case
                 goto E;
                 break;
             }
             printf("Answer: ");
-            n = read(sockfd,&answer,sizeof(int));
-            printf("%d\n",answer );
+            n = read(sockfd,&answer,sizeof(int));       // Getting the answer.
+            printf("%d\n",answer );             // printing the answer
         }
     }
 
--- CNL/Assignment6/server.c
+++ CNL/Assignment6/server.c
@@ -41,23 +41,23 @@ int main(int argc, char const *argv[]) {
         error("Binding Failed.");
     }
 
-    listen(sockfd, 5);
+    listen(sockfd, 5);  // Waiting for client to connect
     clilen = sizeof(cli_addr);
 
-    newsockfd = accept(sockfd,(struct sockaddr *)&cli_addr,&clilen);
+    newsockfd = accept(sockfd,(struct sockaddr *)&cli_addr,&clilen);    // Client accepted
 
     if(newsockfd < 0)
     error("Error accepting");
-    if(portno==9890){
+    if(portno==9890){       // Chat App from server to client and vice versa
         while(1){
             bzero(buffer,255);
-            n = read(newsockfd,buffer,255);
+            n = read(newsockfd,buffer,255);     // Reading from client
             if(n<0)
             error("Error on reading");
             printf("Client: %s\n",buffer);
             bzero(buffer,255);
             fgets(buffer,255,stdin);  // Reading from the server terminal
-            n = write(newsockfd,buffer,strlen(buffer));
+            n = write(newsockfd,buffer,strlen(buffer));     // Sending to CLient
             if(n<0)
             error("Error on writing\n");
             int i = strncmp("bye",buffer,3);
@@ -65,51 +65,60 @@ int main(int argc, char const *argv[]) {
             break;
         }
     }
-    if(portno=9891){
-
+    if(portno==9891){       // File Transfer
+        FILE *fp;
+        int ch =0;
+        fp=  fopen("receivedtextfile.txt","a");     // Append if the file already exists or else create a new one
+        int words;
+        read(newsockfd,&words,sizeof(int));         // Read the word count
+        while (ch!=words) {
+            read(newsockfd,buffer,255);             // Read a word
+            fprintf(fp,"%s ",buffer);               // Write that word to the file
+            ch++;
+        }
+        printf("The file was received");
     }
-    if(portno=9892){
-        printf("It's here alright\n");
+    if(portno==9892){
         int num1,num2,answer,choice;
         char choices[5][15]={"Addition","Subtraction","Multiplication","Division","Exit"};
 
-S:        n = write(newsockfd,"Enter number 1: ",strlen("Enter number 1: "));
+S:        n = write(newsockfd,"Enter number 1: ",strlen("Enter number 1: "));       // Sending a message to ask for num1
         if(n<0)
             error("Error on writing\n");
-        read(newsockfd,&num1,sizeof(int));
+        read(newsockfd,&num1,sizeof(int));      // Reading num1 sent from client
         printf("Client number 1 is: %d\n",num1);
-        n = write(newsockfd,"Enter number 2: ",strlen("Enter number 2: "));
+        n = write(newsockfd,"Enter number 2: ",strlen("Enter number 2: "));         // Sending a message to ask for num2
         if(n<0)
             error("Error on writing\n");
-        read(newsockfd,&num2,sizeof(int));
+        read(newsockfd,&num2,sizeof(int));      // Reading num2 sent from client
         printf("Client number 2 is: %d\n",num2);
 
         n = write(newsockfd,"1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Exit\n",
-                    strlen("1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\5. Exit\n"));
+                    strlen("1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Exit\n"));//Sending request for choice
         if(n<0)
             error("Error on writing\n");
-        read(newsockfd,&choice,sizeof(int));
-        printf("Client operation is: %s\n",choices[choice-1]);
+        read(newsockfd,&choice,sizeof(int));    // Reading choice for operation
+        printf("Client operation is: %s\n",choices[choice-1]);  // Fetching the opearation name from string array
         switch (choice) {
             case 1:
-                answer = num1+num2;
+                answer = num1+num2;     //addition
                 break;
             case 2:
-                answer = num1-num2;
+                answer = num1-num2;     //Subtraction
                 break;
             case 3:
-                answer = num1*num2;
+                answer = num1*num2;     //Multiplication
                 break;
             case 4:
-                answer = num1/num2;
+                answer = num1/num2;     //Division
                 break;
             case 5:
-                goto Q;
+                goto Q;                 //Exit
                 break;
 
         }
         write(newsockfd,&answer,sizeof(int));
-        if(choice!=5){
+        if(choice!=5){      //Exit Case
             goto S;
         }
 
